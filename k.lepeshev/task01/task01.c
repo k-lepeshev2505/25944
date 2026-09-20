e7164269af7386f7547af27ec4ebcd519ce2fd62
Kirill Lepeshev <k.lepeshev@g.nsu.ru>
2026-09-20 16:25:28 +0700

    Test 2 task01

--- k.lepeshev/task01/task01.c
+++ k.lepeshev/task01/task01.c
@@ -3,6 +3,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <errno.h>
+#include <limits.h>
+#include <ulimit.h>
+
+typedef struct Option{
+    char name;
+    char *argument;
+} Option;
 
 
 int main(int argc, char *argv[]){
@@ -12,17 +20,33 @@ int main(int argc, char *argv[]){
     for(int i=1; i < argc; i++)
         capacity += strlen(argv[i]);
 
-    char *options = (char*)malloc(capacity);
+    Option *options = (Option*)malloc(capacity*sizeof(Option));
+
+    if(options == NULL){
+        perror("malloc");
+        return 1;
+    }
+
+
     int idx = 0;
 
     int option = 0;
 
-    while((option = getopt(argc, argv, "ipd")) != -1){
+    while((option = getopt(argc, argv, "ipdsuU:")) != -1){
         switch(option){
             case 'i':
             case 'p':
             case 'd':
-                options[idx++] = (char)option;
+            case 's':
+            case 'u':
+                options[idx].name = option;
+                options[idx].argument = NULL;
+                idx++;
+                break;
+            case 'U':
+                options[idx].name = option;
+                options[idx].argument = optarg;
+                idx++;
                 break;
             case '?':
                 printf("Unknown option\n");
@@ -30,8 +54,9 @@ int main(int argc, char *argv[]){
 
         }
     }
+
     for(int i=idx-1; i >= 0; i--){
-        switch(options[i]){
+        switch(options[i].name){
             case 'i':
                 printf("Real UID: %lu\n", (unsigned long)getuid());
                 printf("Effective UID: %lu\n", (unsigned long)geteuid());
@@ -54,6 +79,42 @@ int main(int argc, char *argv[]){
 
                 break;
             }
+            case 's':
+                if(setpgid(0, 0) == -1)
+                    perror("setpgid");
+                break;
+            case 'u':
+            {
+                long limit = ulimit(UL_GETFSIZE);
+
+                if(limit == -1)
+                    perror("ulimit");
+                else
+                    printf("File size limit: %ld\n", limit);
+
+                break;
+            }
+            case 'U':
+            {
+                char *endptr;
+                
+                errno = 0;
+
+                long new_limit = strtol(options[i].argument, &endptr, 10);
+
+                if(endptr == options[i].argument || *endptr != '\0' || errno == ERANGE || new_limit < 0){
+                    printf("Invalid ulimit value\n");
+                    break;
+                }
+
+                long result = ulimit(UL_SETFSIZE, new_limit);
+
+                if(result == -1)
+                    perror("ulimit");
+
+                break;
+            }
+
         }
     }
 
