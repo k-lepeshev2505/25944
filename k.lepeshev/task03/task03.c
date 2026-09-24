09e357b97c7a0502a306e9786e2ce150b301c43d
Kirill Lepeshev <k.lepeshev@g.nsu.ru>
2026-09-24 10:16:12 +0700

    Test 2 task03

--- k.lepeshev/task03/task03.c
+++ k.lepeshev/task03/task03.c
@@ -5,31 +5,38 @@
 
 int main(){
 
+    printf("First step before setuid\n");
     printf("Real UID: %lu\n", (unsigned long)getuid());
     printf("Effective UID: %lu\n", (unsigned long)geteuid());
 
-    FILE *file_in = fopen("input.txt", "r");
+    FILE *file_in = fopen("vip_file.txt", "r");
 
     if(file_in == NULL){
         perror("open");
     }
-
-    fclose(file_in);
-
+    else{
+        fclose(file_in);
+    }
+    printf("setuid(0) attempt\n");
     if(setuid(0) == -1){
         perror("setuid");
     }
+    else{
+        printf("setuid(0) success\n");
+    }
 
+    printf("Second step after setuid\n");
     printf("Real UID: %lu\n", (unsigned long)getuid());
     printf("Effective UID: %lu\n", (unsigned long)geteuid());
 
-    FILE *file_in = fopen("input.txt", "r");
+    file_in = fopen("vip_file.txt", "r");
 
     if(file_in == NULL){
         perror("open");
     }
-
-    fclose(file_in);
+    else{
+        fclose(file_in);
+    }
 
 
     exit(0);
